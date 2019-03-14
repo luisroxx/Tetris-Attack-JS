2fed9587f9cbf201538e7622d17cf63302454fe6
Luis Guilherme da Costa Martins <Luis_roxx@hotmail.com>
2019-03-14 19:00:26 -0300

    -14/03/2019
    - Arrumado movimentacao invertida

--- resources/js/EndlessMode.js
+++ resources/js/EndlessMode.js
@@ -55,7 +55,11 @@ let CURSOR = {
             frameOrder: [0],
             frameTime: [1000]
         }
-    ]
+    ],
+    position: {
+        x: 0,
+        y: 0
+    }
 };
 
 let GREEN_BLOCK = {
@@ -85,146 +89,175 @@ let GREEN_BLOCK = {
     ]
 };
 
-let addLineOffsetY;
-let blocks;
+let blocks = [];
 
 function startEndlessMode() {
     blocks = new Array();
-    addLineOffsetY = 0;
     let anims = [];
     anims = anims.concat(BG_YOSHI.animations);
     anims = anims.concat(GREEN_BLOCK.animations);
     anims = anims.concat(CURSOR.animations);
     preCacheAnimations(anims, createEndlessMode);
-};
+}
+;
+
 
 function createEndlessMode() {
     startBG();
     addLine();
     startCursor();
     //startTimerIntro().then(StartGameLogic());
-    setInterval(moveObjects,1000);
+    setInterval(startGameLogic, 100);
 }
 
-window.onkeydown = function(e)
-{
-        code = e.keyCode;
+let loops = 1;
+function startGameLogic(){
+    moveObjects();
+    if (loops === 16){
+        addLine();
+        loops = 0;
+        //alert();
+    }
+    loops++;
+}
 
-	switch(code){
-//		case 40://baixo
-//                    
-//			movimentar(jogo, cursor, 0, 1, tabuleiro.dimensoes)
-//		break;
-		case 39://direita,
-                        if(x>blocks.length-3) return;
-                        x++;
-                        movimentarCursor(CURSOR.animations[0].el);
-		break;
-//		case 38://cima
-//			movimentar(jogo, cursor, 0, -1, tabuleiro.dimensoes)
-//		break;
-		case 37://esquerda
-                     if(x<1) return;
-                        x--;
-                        movimentarCursor(CURSOR.animations[0].el);
-		break;
-//		case 32://space
+window.onkeydown = function (e)
+{
+    code = e.keyCode;
+
+    switch (code) {
+        case 40://baixo
+            if (CURSOR.position.y === 0)
+                return;
+            CURSOR.position.y--;
+            movimentarCursor(CURSOR.animations[0].el);
+            break;
+        case 39://direita,
+            if (CURSOR.position.x === blocks[0].length - 2)
+                return;
+            CURSOR.position.x++;
+            movimentarCursor(CURSOR.animations[0].el);
+            break;
+        case 38: //cima
+            if (CURSOR.position.y === blocks.length-1)
+                return;
+            CURSOR.position.y++;
+            movimentarCursor(CURSOR.animations[0].el);
+            break;
+        case 37://esquerda
+            if (CURSOR.position.x === 0)
+                return;
+            CURSOR.position.x--;
+            movimentarCursor(CURSOR.animations[0].el);
+            break;
+        case 32://space
+            addLine();
 //			if(switchBlock(jogo, cursor))
 //				checkCombinacoes(jogo);
-//		break;
-	}
+            break;
+    }
 }
 
 
 
-function startBG(){
-    rescaleGameWindow(BG_YOSHI.animations[0].el,scaledSize);
+function startBG() {
+    rescaleGameWindow(BG_YOSHI.animations[0].el, scaledSize);
     vieweEndlessMode.style.position = "relative";
-    vieweEndlessMode.style.background = "url("+BG_YOSHI.animations[0].el.src+")" ;
+    vieweEndlessMode.style.background = "url(" + BG_YOSHI.animations[0].el.src + ")";
     vieweEndlessMode.style.backgroundrepeat = "no-repeat";
-    vieweEndlessMode.style.backgroundSize =  "cover";
-};
+    vieweEndlessMode.style.backgroundSize = "cover";
+}
+;
 
-function startCursor(){
+function startCursor() {
     rescaleObj(CURSOR.animations[0].el);
     vieweEndlessMode.appendChild(CURSOR.animations[0].el);
     posCursor(CURSOR.animations[0].el);
-};
+    CURSOR.position.x = 0;
+    CURSOR.position.y = 0;
+}
+;
 
-function posCursor(cursor){
+function posCursor(cursor) {
     cursor.style.position = "absolute";
     cursor.style.zIndex = "5";
     let gameWidth = (Game.offsetWidth);
     let gameHeight = (Game.offsetHeight);
     //cursor.style.top = -(cursor.height)+gameHeight*0.959641+"px";
     //cursor.style.left = gameWidth*0.34+"px";
-    cursor.style.left = getLeft(blocks[0].animations[0].el)-(cursor.width*0.05)+"px";
-    cursor.style.top =  getTop(blocks[0].animations[0].el)-(cursor.height*0.1)+"px";
-}
-let x = 0;
-function movimentarCursor(cursor){
-    cursor.style.left = getLeft(blocks[x].animations[0].el)-(cursor.width*0.05)+"px";
-    cursor.style.top =  getTop(blocks[x].animations[0].el)-(cursor.height*0.1)+"px";
+    cursor.style.left = getLeft(blocks[0][0].animations[0].el) - (cursor.width * 0.05) + "px";
+    cursor.style.top = getTop(blocks[0][0].animations[0].el) - (cursor.height * 0.1) + "px";
 }
 
-function moveObjects(){
-    CURSOR.animations[0].el.style.top = (getTop(CURSOR.animations[0].el)-scaledSize)+"px";
-    for (let i = 0; i<blocks.length;i++){
-        blocks[i].animations[0].el.style.top = (getTop(blocks[i].animations[0].el)-scaledSize)+"px";
+function movimentarCursor(cursor) {
+    cursor.style.left = getLeft(blocks[CURSOR.position.y][CURSOR.position.x].animations[0].el) - (cursor.width * 0.05) + "px";
+    cursor.style.top = getTop(blocks[CURSOR.position.y][CURSOR.position.x].animations[0].el) - (cursor.height * 0.1) + "px";
+}
+
+function moveObjects() {
+    CURSOR.animations[0].el.style.top = (getTop(CURSOR.animations[0].el) - scaledSize) + "px";
+    for (let y = 0; y < blocks.length; y++) {
+        for (let x = 0; x < blocks[y].length; x++) {
+            blocks[y][x].animations[0].el.style.top = (getTop(blocks[y][x].animations[0].el) - scaledSize) + "px";
+        }
     }
-    //addLineOffsetY-=GREEN_BLOCK.animations[0].el.width;
 }
 
 
 
-function getTop(el){
+function getTop(el) {
     var top = el.style.top;
-    return parseInt(top.replace("px",""));
+    return parseInt(top.replace("px", ""));
 }
 
-function getLeft(el){
+function getLeft(el) {
     var top = el.style.left;
-    return parseInt(top.replace("px",""));
+    return parseInt(top.replace("px", ""));
 }
 
-function addLine(){
-    createNewLine(addLineOffsetY);
-    //addLineOffsetY-=GREEN_BLOCK.animations[0].el.width;
-}
-
-function createNewLine(offsetY){
+function addLine() {
     let offsetX = 0;
-    for (let i=0; i<6;i++){
-        offsetX += createPiece(offsetX,offsetY);
+    let newLine = [];
+    for (let i = 0; i < 6; i++) {
+        offsetX += createPiece(offsetX, newLine);
+    }
+    blocks.push(newLine);
+    let aux = blocks;
+    CURSOR.position.y++;
+    blocks = [];
+    blocks.push(newLine);
+    for (let i=0;i< aux.length-1;i++){
+        console.log(aux.length);
+        blocks.push(aux[i]);
     }
 }
 
-function createPiece(offsetX,offsetY){
+function createPiece(offsetX, newLine) {
     let piece = copy(GREEN_BLOCK);
     rescaleObj(piece.animations[0].el);
     vieweEndlessMode.appendChild(piece.animations[0].el);
-    posPiece(piece.animations[0].el, offsetX,offsetY);
-    blocks.push(piece);
+    posPiece(piece.animations[0].el, offsetX);
+    newLine.push(piece);
     return piece.animations[0].el.width;
 }
 
-function rescaleGameWindow(bgImg){
-    Game.style.height = bgImg.height*scaledSize+"px";
-    Game.style.width = bgImg.width*scaledSize+"px";
+function rescaleGameWindow(bgImg) {
+    Game.style.height = bgImg.height * scaledSize + "px";
+    Game.style.width = bgImg.width * scaledSize + "px";
 }
 
-function rescaleObj(obj){
-    obj.style.height = obj.height*scaledSize+"px";
-    obj.style.width = obj.width*scaledSize+"px";
+function rescaleObj(obj) {
+    obj.style.height = obj.height * scaledSize + "px";
+    obj.style.width = obj.width * scaledSize + "px";
 }
 
-function posPiece(piece, offsetX,offsetY){
+function posPiece(piece, offsetX, offsetY) {
     piece.style.position = "absolute";
     let gameWidth = (Game.offsetWidth);
     let gameHeight = (Game.offsetHeight);
     //piece.style.left = (piece.width*2.5)+(-gameWidth/2)+(piece.width/2)+gameWidth*0.345+"px";
-    piece.style.left = offsetX+gameWidth*0.345+"px";
-    piece.style.top = offsetY-(piece.height)+gameHeight*0.959641+"px";
+    piece.style.left = offsetX + gameWidth * 0.345 + "px";
+    piece.style.top = -(piece.height) + gameHeight * 0.959641 + "px";
 }
 
 function copy(o) {
--- resources/js/Game.js
+++ resources/js/Game.js
@@ -5,21 +5,45 @@
  */
 
 let Game = document.getElementById("game");
-const scaledSize = 1;
+const scaledSize = 3;
 
 function start() {
     //preCacheAnimations(TodasAnimacoes,startViewe(VIEWES.SINGLEPLAYER););// quando tudo tiver pronto
     startViewe(VIEWES.ENDLESSMODE);
 };
 
+let content = document.getElementById("content");
+
 function openFullscreen() {
-    if (Game.requestFullscreen) {
-        Game.requestFullscreen();
-    } else if (Game.mozRequestFullScreen) { /* Firefox */
-        Game.mozRequestFullScreen();
-    } else if (Game.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-        Game.webkitRequestFullscreen();
-    } else if (Game.msRequestFullscreen) { /* IE/Edge */
-        Game.msRequestFullscreen();
+    if (content.requestFullscreen) {
+        content.requestFullscreen();
+    } else if (content.mozRequestFullScreen) { /* Firefox */
+        content.mozRequestFullScreen();
+    } else if (content.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+        
+        content.webkitRequestFullscreen();
+    } else if (content.msRequestFullscreen) { /* IE/Edge */
+        content.msRequestFullscreen();
     }
 };
+
+let tempH;
+let tempW;
+
+document.addEventListener("fullscreenchange", function(event) {
+  if (document.fullscreenElement) {
+        tempH = Game.style.height;
+        tempW = Game.style.width;
+        Game.style.height = "100%";
+        alert(Game.offsetHeight);
+        alert(Game.getBoundingClientRect().width);
+        Game.style.margin = "0 auto";
+        Game.style.width = "5em";
+      //alert("change");
+    // The browser is in full-screen mode, with document.fullscreenElement
+    // being the element presented full-screen.
+  } else {
+    Game.style.height = tempH;
+    Game.style.width = tempW;
+  }
+});
